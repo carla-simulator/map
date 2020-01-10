f2f1c688d48eb4a2f3058ba17cb13432da1f7c54
Fabian Oboril <fabian.oboril@intel.com>
2020-01-10 10:16:30 +0100

    Removed tools from Travis-CI build

--- cmake/compile-options.cmake
+++ cmake/compile-options.cmake
@@ -7,5 +7,3 @@
 # ----------------- END LICENSE BLOCK -----------------------------------
 
 option(BUILD_SHARED_LIBS "Build shared libraries instead of static ones" ON)
-option(BUILD_QGIS_PLUGIN "Build plugin for visualizing maps in QGIS" OFF)
-option(BUILD_PYTHON "Build python binding" OFF)
