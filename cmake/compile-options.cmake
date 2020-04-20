a429b667b1f9ba677d1d839d77cef11f320e0a98
Fabian Oboril <fabian.oboril@intel.com>
2020-04-20 15:56:01 +0200

    Cleanup documentation

--- cmake/compile-options.cmake
+++
@@ -1,7 +0,0 @@
-# ----------------- BEGIN LICENSE BLOCK ---------------------------------
-#
-# Copyright (C) 2019 Intel Corporation
-#
-# ----------------- END LICENSE BLOCK -----------------------------------
-
-option(BUILD_SHARED_LIBS "Build shared libraries instead of static ones" ON)
--- tools/map_maker/osm_converter/doc/README.md
+++
@@ -1,5 +0,0 @@
-# osm_converter
-
-The osm_converter reads in OSM data and processes the data to be used by any converter
-tool to produce other output formats
-
