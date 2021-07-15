8b85e9dbe83f1771518165d2ec8e6bc7f10eecf9
Joel Moriana <joel.moriana@gmail.com>
2021-07-15 08:03:31 +0200

    first iteration python wheels

---
+++ ad_map_access/python/setup.py
@@ -0,0 +1,31 @@
+import sys
+
+from setuptools import setup
+from setuptools.command.install import install as _install
+from setuptools.dist import Distribution
+
+
+class BinaryDistribution(Distribution):
+    """Distribution which always forces a binary package with platform name"""
+    def has_ext_modules(self):
+        return True
+
+
+class Install(_install):
+    def finalize_options(self):
+        _install.finalize_options(self)
+        if self.distribution.has_ext_modules():
+            self.install_lib = self.install_platlib
+
+
+setup(
+    name="ad-map-access",
+    packages=["ad_map_access"],
+    package_dir={"": sys.argv.pop(-1)},
+    package_data={"ad_map_access": ["*.so"]},
+    version="2.4.5",
+    description="AD Map access library ",
+    install_requires=["ad_physics>=2.4.5"],
+    distclass=BinaryDistribution,
+    cmdclass={'install': Install}
+)
---
+++ ad_physics/python/setup.py
@@ -0,0 +1,30 @@
+import sys
+
+from setuptools import setup
+from setuptools.command.install import install as _install
+from setuptools.dist import Distribution
+
+
+class BinaryDistribution(Distribution):
+    """Distribution which always forces a binary package with platform name"""
+    def has_ext_modules(self):
+        return True
+
+
+class Install(_install):
+    def finalize_options(self):
+        _install.finalize_options(self)
+        if self.distribution.has_ext_modules():
+            self.install_lib = self.install_platlib
+
+
+setup(
+    name="ad-physics",
+    packages=["ad_physics"],
+    package_dir={"": sys.argv.pop(-1)},
+    package_data={"ad_physics": ["*.so"]},
+    version="2.4.5",
+    description="AD Map access library ",
+    distclass=BinaryDistribution,
+    cmdclass={'install': Install}
+)
