95a2e24c9a6a8a6be932eef3abfc6888b9375bc6
Bernd Gassmann <bernd.gassmann@intel.com>
2025-07-02 17:19:22 +0200

    Try to cover Ubuntu24.04 and python3.11 in build

--- .github/workflows/install_dependencies.sh
+++ .github/workflows/install_dependencies.sh
@@ -1,25 +1,36 @@
 #!/bin/bash
 
-sudo apt-get update
-sudo apt-get install -y lsb-core
-sudo apt-get install -y --no-install-recommends build-essential castxml cmake libgtest-dev liblapacke-dev libopenblas-dev libpugixml-dev sqlite3
-
-IS_PYTHON_3_10=0
-echo "!!!!!!! Python version: ${PYTHON_BINDING_VERSION} !!!!!!!"
-if [ "${PYTHON_BINDING_VERSION}" == "3.10" ]; then
-  echo "!!!!!!! Python version is 3.10 detected !!!!!!!"
-  IS_PYTHON_3_10=1
-fi
-
+IS_DEFAULT_PYTHON_VERSION=1
 if [ `lsb_release -a | grep Release | grep "22.04" | wc -l` == 1 ]; then
   if [ "${PYTHON_BINDING_VERSION}" == "" ]; then
     echo "!!!!!!! Setting default python version for Ubuntu22.04 !!!!!!!"
-    IS_PYTHON_3_10=1
     PYTHON_BINDING_VERSION="3.10"
+  elif [ "${PYTHON_BINDING_VERSION}" != "3.10" ]; then
+    IS_DEFAULT_PYTHON_VERSION=0
   fi
 fi
 
-sudo apt-get install -y --no-install-recommends libboost-all-dev
+if [ `lsb_release -a | grep Release | grep "24.04" | wc -l` == 1 ]; then
+  if [ "${PYTHON_BINDING_VERSION}" == "" ]; then
+    echo "!!!!!!! Setting default python version for Ubuntu24.04 !!!!!!!"
+    PYTHON_BINDING_VERSION="3.12"
+  elif [ "${PYTHON_BINDING_VERSION}" != "3.12" ]; then
+    IS_DEFAULT_PYTHON_VERSION=0
+  fi
+fi
+
+echo "!!!!!!! Python version: ${PYTHON_BINDING_VERSION} !!!!!!!"
+
+if ((! IS_DEFAULT_PYTHON_VERSION)); then
+  sudo add-apt-repository ppa:deadsnakes/ppa
+fi
+
+sudo apt-get update
+
+sudo apt-get install -y lsb-core
+sudo apt-get install -y --no-install-recommends build-essential castxml cmake sqlite3 libgtest-dev libboost-all-dev libpugixml-dev libgtest-dev liblapacke-dev libspdlog-dev
+
+
 
 sudo apt-get install -y --no-install-recommends python${PYTHON_BINDING_VERSION}-dev libpython${PYTHON_BINDING_VERSION}-dev
 curl -sS https://bootstrap.pypa.io/get-pip.py | sudo python${PYTHON_BINDING_VERSION}
--- README.md
+++ README.md
@@ -76,6 +76,7 @@ Following compiler and Python combinations are [tested continously](.github/work
 |  Clang 14       |       x      |              |
 |  Clang 18       |              |       x      |
 |  Python 3.10    |       x      |              |
+|  Python 3.11    |       x      |       x      |
 |  Python 3.12    |              |       x      |
 
 Important: cmake is required to be at least version 3.10!
