ed264a3969f5e74a2aad04c5822211cf362d8c20
Bernd Gassmann <bernd.gassmann@intel.com>
2025-07-02 18:45:28 +0200

    Compile boost for non system default python

--- .github/workflows/install_dependencies.sh
+++ .github/workflows/install_dependencies.sh
@@ -1,5 +1,44 @@
 #!/bin/bash
 
+install_boost() {
+  if ((IS_DEFAULT_PYTHON_VERSION)); then
+    echo "!!!!!!! The default python version $(which python${PYTHON_BINDING_VERSION}): install system boost !!!!!!!"
+    sudo apt-get install -y --no-install-recommends libboost-all-dev
+  else
+    echo "!!!!!!! Not the default python version $(which python${PYTHON_BINDING_VERSION}): compile boost 1.83 !!!!!!!"
+    pushd dependencies
+
+    BOOST_VERSION=1.83.0
+    BOOST_PACKAGE_BASENAME=boost_${BOOST_VERSION//./_}
+    wget "https://boostorg.jfrog.io/artifactory/main/release/${BOOST_VERSION}/source/${BOOST_PACKAGE_BASENAME}.tar.gz"
+    wget "https://archives.boost.io/release/${BOOST_VERSION}/source/${BOOST_PACKAGE_BASENAME}.tar.gz"
+
+    tar -xzf ${BOOST_PACKAGE_BASENAME}.tar.gz
+    pushd ${BOOST_PACKAGE_BASENAME}
+
+    py3=`which python${PYTHON_BINDING_VERSION}`
+    py3_root=`${py3} -c "import sys; print(sys.prefix)"`
+    pyv=`$py3 -c "import sys;x='{v[0]}.{v[1]}'.format(v=list(sys.version_info[:2]));sys.stdout.write(x)";`
+
+    ./bootstrap.sh \
+      --prefix="/usr" \
+      --with-libraries=python,filesystem,system,program_options \
+      --with-python=${py3} --with-python-version=${pyv} --with-python-root=${py3_root}
+    if (( $? )); then
+      echo "!!!!!!! boost bootstrap failed !!!!!!!"
+      cat bootstrap.log
+    fi
+
+    ./b2 --prefix="/usr" cxxflags="-fPIC" -j 10 stage release
+    sudo ./b2 --prefix="/usr" cxxflags="-fPIC" -j 10 install
+
+    # ensure our colcon build process ignores boost sources
+    touch COLCON_IGNORE
+
+    popd
+  fi
+}
+
 IS_DEFAULT_PYTHON_VERSION=1
 if [ `lsb_release -a | grep Release | grep "22.04" | wc -l` == 1 ]; then
   if [ "${PYTHON_BINDING_VERSION}" == "" ]; then
@@ -28,7 +67,7 @@ fi
 sudo apt-get update
 
 sudo apt-get install -y lsb-core
-sudo apt-get install -y --no-install-recommends build-essential castxml cmake sqlite3 libgtest-dev libboost-all-dev libpugixml-dev libgtest-dev liblapacke-dev libspdlog-dev
+sudo apt-get install -y --no-install-recommends build-essential castxml cmake sqlite3 libgtest-dev libpugixml-dev libgtest-dev liblapacke-dev libspdlog-dev
 
 
 
@@ -46,5 +85,5 @@ python${PYTHON_BINDING_VERSION} -m venv map-build-venv
 source map-build-venv/bin/activate && \
 echo "activating virtual environment at $(which python${PYTHON_BINDING_VERSION})" && \
 curl -sS https://bootstrap.pypa.io/get-pip.py | python${PYTHON_BINDING_VERSION}  && \
-python${PYTHON_BINDING_VERSION} -m pip install -r .github/workflows/requirements.txt
-
+python${PYTHON_BINDING_VERSION} -m pip install -r .github/workflows/requirements.txt && \
+install_boost
